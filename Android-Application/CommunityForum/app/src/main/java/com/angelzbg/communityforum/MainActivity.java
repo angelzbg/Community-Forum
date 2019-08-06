2a7d5fde83f153f30508ffdac23cb6725e8d5203
angelzbg <badblo0dbg@gmail.com>
2019-08-06 12:09:25 +0300

    little update

--- Android-Application/CommunityForum/app/src/main/java/com/angelzbg/communityforum/MainActivity.java
+++ Android-Application/CommunityForum/app/src/main/java/com/angelzbg/communityforum/MainActivity.java
@@ -1,138 +1,106 @@
 package com.angelzbg.communityforum;
 
-import android.graphics.Color;
-import android.support.annotation.NonNull;
+import android.content.res.Resources;
+import android.media.MediaPlayer;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.angelzbg.communityforum.utils.SoundHelper;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private long topDate = 1564591148286L, bottomDate = 1564591148286L;
+    // Firebase
+    private DatabaseReference dbRootReference;
+
+    // Display metrix
+    private int width, height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference("test");
+        // Firebase
+        dbRootReference = FirebaseDatabase.getInstance().getReference();
 
+        // Display metrix
+        width  = Resources.getSystem().getDisplayMetrics().widthPixels;
+        height = Resources.getSystem().getDisplayMetrics().heightPixels;
+        if(width > height){
+            int temp = width;
+            width = height;
+            height = temp;
+        }
 
-        final ScrollView sv_test = findViewById(R.id.sv_test);
-        final LinearLayout ll_test = findViewById(R.id.ll_test);
+        /* Resizing the Main Menu [ START ] */
+        findViewById(R.id.main_CL_MainMenu).getLayoutParams().height = (int) (height / 13.33);
+        findViewById(R.id.main_CL_MainMenu).setPadding(height/80, height/80, height/80, height/80);
+        findViewById(R.id.main_CL_MainMenu).setElevation(height/80); // другите /160 !
+        /* Resizing the Main Menu [  END  ] */
 
-        myRef.orderByChild("date").endAt(bottomDate).limitToLast(5).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot DATA) {
-                if(DATA.exists()){
+        /* ImageButtons MainMenu onClickListeners [ START ] */
+        findViewById(R.id.main_IB_Drawer_MainMenu).setOnClickListener(menuSwitcher);
+        findViewById(R.id.main_IB_Saved_MainMenu).setOnClickListener(menuSwitcher);
+        findViewById(R.id.main_IB_Home_MainMenu).setOnClickListener(menuSwitcher);
+        findViewById(R.id.main_IB_Notifications_MainMenu).setOnClickListener(menuSwitcher);
+        findViewById(R.id.main_IB_Search_MainMenu).setOnClickListener(menuSwitcher);
+        /* ImageButtons MainMenu onClickListeners [  END  ] */
 
-                    List<TestData> list = new ArrayList<>();
-
-                    for(DataSnapshot dataSnapshot : DATA.getChildren()){
-                        list.add(dataSnapshot.getValue(TestData.class));
-                    }
-
-                    Collections.reverse(list);
-
-                    for(TestData td : list){
-                        TextView TV = new TextView(getApplicationContext());
-                        TV.setId(View.generateViewId());
-                        TV.setTextColor(Color.BLACK);
-                        TV.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 100);
-                        TV.setText(td.getName());
-                        ll_test.addView(TV);
-                        bottomDate = td.getDate();
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-
-        sv_test.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
-            @Override
-            public void onScrollChanged() {
-                if (sv_test.getHeight() == sv_test.getChildAt(0).getHeight() - sv_test.getScrollY()) {
-                    //scroll view is at the bottom
-                    myRef.orderByChild("date").endAt(bottomDate-1).limitToLast(5).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot DATA) {
-                            if(DATA.exists()){
-
-                                List<TestData> list = new ArrayList<>();
-
-                                for(DataSnapshot dataSnapshot : DATA.getChildren()){
-                                    list.add(dataSnapshot.getValue(TestData.class));
-                                }
-
-                                Collections.reverse(list);
-
-                                for(TestData td : list){
-                                    TextView TV = new TextView(getApplicationContext());
-                                    TV.setId(View.generateViewId());
-                                    TV.setTextColor(Color.BLACK);
-                                    TV.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 100);
-                                    TV.setText(td.getName());
-                                    ll_test.addView(TV);
-                                    bottomDate = td.getDate();
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
-                } else if(sv_test.getScrollY() == 0) {
-                    //scroll view is at the top
-                    myRef.orderByChild("date").startAt(topDate+1).limitToFirst(5).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot DATA) {
-                            if(DATA.exists()){
-                                for (DataSnapshot dataSnapshot : DATA.getChildren()) {
-                                    TestData td = dataSnapshot.getValue(TestData.class);
-                                    TextView TV = new TextView(getApplicationContext());
-                                    TV.setId(View.generateViewId());
-                                    TV.setTextColor(Color.BLACK);
-                                    TV.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 100);
-                                    TV.setText(td.getName());
-                                    ll_test.addView(TV, 0);
-                                    topDate = td.getDate();
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
-                }
-            }
-        });
     } // onCreate()
+
+    /* MenuSwitch [ START ] */
+    View.OnClickListener menuSwitcher = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.main_IB_Drawer_MainMenu:
+                    // open Drawer
+                    break;
+                case R.id.main_IB_Saved_MainMenu:
+                    findViewById(R.id.main_SV_Posts).setVisibility(View.INVISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Home_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkerGray));
+
+                    findViewById(R.id.main_SV_Notifications).setVisibility(View.INVISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Notifications_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkerGray));
+
+                    findViewById(R.id.main_SV_SavedPosts).setVisibility(View.VISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Saved_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
+                    SoundHelper.playSound(getApplicationContext(), SoundHelper.TYPE_MENU_SWITCH);
+                    break;
+                case R.id.main_IB_Home_MainMenu:
+                    findViewById(R.id.main_SV_Notifications).setVisibility(View.INVISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Notifications_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkerGray));
+
+                    findViewById(R.id.main_SV_SavedPosts).setVisibility(View.INVISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Saved_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkerGray));
+
+                    findViewById(R.id.main_SV_Posts).setVisibility(View.VISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Home_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
+                    SoundHelper.playSound(getApplicationContext(), SoundHelper.TYPE_MENU_SWITCH);
+                    break;
+                case R.id.main_IB_Notifications_MainMenu:
+                    findViewById(R.id.main_SV_SavedPosts).setVisibility(View.INVISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Home_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkerGray));
+
+                    findViewById(R.id.main_SV_Posts).setVisibility(View.INVISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Saved_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkerGray));
+
+                    findViewById(R.id.main_SV_Notifications).setVisibility(View.VISIBLE);
+                    ((ImageButton)findViewById(R.id.main_IB_Notifications_MainMenu)).setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
+                    SoundHelper.playSound(getApplicationContext(), SoundHelper.TYPE_MENU_SWITCH);
+                    break;
+                case R.id.main_IB_Search_MainMenu:
+                    // go to search activity
+                    break;
+            }
+        }
+    };
+    /* MenuSwitch [  END  ] */
 
 
 
---
+++ Android-Application/CommunityForum/app/src/main/java/com/angelzbg/communityforum/utils/SoundHelper.java
@@ -0,0 +1,42 @@
+package com.angelzbg.communityforum.utils;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import com.angelzbg.communityforum.R;
+
+public final class SoundHelper {
+
+    public static final int TYPE_MENU_SWITCH = 1;
+
+    public static void playSound(final Context context, final int type) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int resId = -1;
+                switch (type) {
+                    case TYPE_MENU_SWITCH:
+                        resId = R.raw.menu_switch;
+                        break;
+                }
+
+                if (resId != -1) {
+                    final MediaPlayer mediaPlayer = MediaPlayer.create(context, resId);
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            mediaPlayer.stop();
+                            mediaPlayer.release();
+                        }
+                    });
+                    mediaPlayer.setLooping(false);
+                    mediaPlayer.start();
+
+                    while (mediaPlayer.isPlaying() == true) {
+                    }
+                }
+            }
+        }).start();
+    } // playSound()
+
+} // SoundHelper{}
